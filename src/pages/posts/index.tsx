17253a33e649c3fef2487e620fcba422f3f5ee4d
meta-crawler <johncarlosoft@gmail.com>
2023-09-14 01:16:47 +0200

    feat: minor update in Post page

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -14,24 +14,26 @@ export default function Post({ post }: IPostProps) {
   };
 
   return (
-    <div className="h-fit flex-1 rounded-lg shadow-primary md:h-[420px]">
-      <img
-        src={post.imageUrl}
-        alt={post.title}
-        className="h-fit rounded-t-lg md:h-56"
-      />
+    post && (
+      <div className="h-fit flex-1 rounded-lg shadow-primary md:h-[420px]">
+        <img
+          src={post.imageUrl}
+          alt={post.title}
+          className="h-fit rounded-t-lg md:h-56"
+        />
 
-      <div className="flex flex-col space-y-3 p-2 lg:p-4">
-        <div className="flex w-full flex-row flex-wrap gap-2">
-          {categories &&
-            post.categories &&
-            post.categories.map((category) => (
-              <Category key={category} category={getCategoryById(category)} />
-            ))}
+        <div className="flex flex-col space-y-3 p-2 lg:p-4">
+          <div className="flex w-full flex-row flex-wrap gap-2">
+            {categories &&
+              post.categories &&
+              post.categories.map((category) => (
+                <Category key={category} category={getCategoryById(category)} />
+              ))}
+          </div>
+          <p className="text-sm font-semibold text-gray-600">{post.title}</p>
+          <p className="text-xs font-medium text-gray-400">{post.excerpt}</p>
         </div>
-        <p className="text-sm font-semibold text-gray-600">{post.title}</p>
-        <p className="text-xs font-medium text-gray-400">{post.excerpt}</p>
       </div>
-    </div>
+    )
   );
 }
